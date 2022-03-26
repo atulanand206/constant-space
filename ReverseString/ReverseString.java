4e6c7615f6762bcf25f4247af952cbb421b832ae
Atul <atulanand206@gmail.com>
2022-03-26 08:36:36 +0530

    Reverse String O(1)

---
+++ ReverseString/ReverseString.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class ReverseString {
+
+	/**
+	 * Languages like C++ and Golang represent strings as a char array
+	 * and are mutable but strings are immutable in Java and it would 
+	 * be impossible to alter it w/o using a StringBuilder or a char array
+	 * 
+	 * For the purposes of this explanation, a string would be 
+	 * represented using a char array
+	 *
+	 * The method would not return anything rather modify the given
+	 * char array in-place.
+	 */
+	public void reverseString(char[] characters) {
+		int i = 0, j = characters.length - 1;
+		while (i < j) {
+			char temp = characters[i];
+			characters[i] = characters[j];
+			characters[j] = temp;
+			i++;
+			j--;
+		}
+	}
+
+	/**
+	 * Converts a given char array to a String using StringBuilder
+	 * and gets it ready for printing in one go.
+	 */
+	private static String stringify(char[] chars) {
+		StringBuilder sb = new StringBuilder();
+		for (char ch : chars) {
+			sb.append(ch);
+		}
+		return sb.toString();
+	}
+
+  public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char[] chars = br.readLine().toCharArray();
+		new ReverseString().reverseString(chars);
+		System.out.println(stringify(chars));		
+  }
+}
